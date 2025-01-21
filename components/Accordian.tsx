d6a47a23149317684befa5c7030a051ef4727e30
wrightsam489 <wrightsam489@gmail.com>
2025-01-20 23:46:30 -0600

    Added basic outline of home screen with new reusable components

---
+++ components/Accordian.tsx
@@ -0,0 +1,80 @@
+import { PropsWithChildren, useState } from "react";
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Header from './Header';
+import Game, { GameData } from "./Game";
+
+type AccordianProps = PropsWithChildren<{
+  title: string;
+  isInitiallyOpen?: boolean | undefined
+}>;
+
+const DATA: GameData[] = [
+    {
+      id: '1',
+      title: 'First Item',
+    },
+    {
+      id: '2',
+      title: 'Second Item',
+    },
+    {
+      id: '3',
+      title: 'Third Item',
+    },
+    {
+      id: '4',
+      title: 'First Item',
+    },
+    {
+      id: '5',
+      title: 'Second Item',
+    },
+    {
+      id: '6',
+      title: 'Third Item',
+    },
+];
+
+function Accordian({title, isInitiallyOpen = false}: AccordianProps) {
+    const [isOpen, setIsOpen] = useState(isInitiallyOpen);
+    const [selectedId, setSelectedId] = useState<string>();
+
+    const renderItem = ({item}: {item: GameData}) => {
+        const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
+        const color = item.id === selectedId ? 'white' : 'black';
+    
+        return (
+          <Game
+            item={item}
+            onPress={() => setSelectedId(item.id)}
+            backgroundColor={backgroundColor}
+            textColor={color}
+          />
+        );
+    }
+
+    return (
+        <View style={styles.accordian}>
+            <TouchableOpacity onPress={() => {setIsOpen(!isOpen)}}>
+                <Header title={title} />
+            </TouchableOpacity >
+            { isOpen &&
+                <FlatList 
+                    horizontal
+                    data={DATA}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                    extraData={selectedId}
+                />
+            }
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    accordian: {
+        marginBottom: 20,
+    },
+});
+
+export default Accordian;
